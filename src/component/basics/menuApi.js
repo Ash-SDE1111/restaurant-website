5d26f517242b64b2c08344267ae00b7162973f7b
Ash-SDE1111 <saxenaashwarya1111@gmail.com>
2023-03-19 13:50:56 -0700

    secondcommit

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "BREAKFAST",
+      price: "12₹",
+      description:
+        "  For a high-protein noodle dish, cook the Maggi noodles with a beaten egg and toss the cooked mixture with lots of grated cheese. If you want noodles with a spicy kick, heat the noodles with chilies, garlic, and hot sauce. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "EVENING",
+      price: "20₹",
+      description:
+        "Aloo pakora or potato bajji are crisp potato fritters which are deep fried ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "BREAKFAST",
+      price: "10₹",
+      description:
+        "Sweet corn chaat is a delectable spiced and tangy snack made with steamed or boiled sweet corn and spices",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "LUNCH",
+      price: "50₹",
+      description:
+        "This chole masala is made with freshly powdered spices, onions, tomatoes, herbs making the chickpea curry taste  ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "EVENING",
+      price: "80₹",
+      description:
+        "We have 4 varities of pizza - corn pizza, magrita pizza, capcicum pizza and onion pizza ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "DINNER",
+      price: "180₹",
+      description:
+        "A typical non-veg meal includes meat or fish in it along with other vegetarian dishes.", 
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "DINNER",
+      price: "60₹",
+      description:
+        "We have jalebi, icecream scoops,icecream rolls, gajar ka halwa, moongdal halwa, speaciality is paan flavor icecream ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "LUNCH",
+      price: "60₹",
+      description:
+        "We serve punjab style rajmachawal. It is spicy and full of flavors.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "EVENING",
+      price: "10₹",
+      description:
+        " We have aloo samosa, shejwaan samosa, paneer samosa, samosa chaat, chowmin samosa",
+    },
+  ];
+  
+  export default Menu;
